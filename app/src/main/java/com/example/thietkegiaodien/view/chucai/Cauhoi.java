4f45be8841047a2335561f5b1513053ed1ae8b22
Do Trung Hieu <20020404@vnu.edu.vn>
2022-12-11 09:14:22 +0700

    dm e quang

---
+++ app/src/main/java/com/example/thietkegiaodien/view/chucai/Cauhoi.java
@@ -0,0 +1,52 @@
+package com.example.thietkegiaodien.view.chucai;
+
+public class Cauhoi {
+    private int noidung;
+    private String dapan1;
+    private String dapan2;
+    private String dapan3;
+    private String dapan4;
+
+    public Cauhoi() {
+    }
+
+    public int getNoidung() {
+        return noidung;
+    }
+
+    public void setNoidung(int noidung) {
+        this.noidung = noidung;
+    }
+
+    public String getDapan1() {
+        return dapan1;
+    }
+
+    public void setDapan1(String dapan1) {
+        this.dapan1 = dapan1;
+    }
+
+    public String getDapan2() {
+        return dapan2;
+    }
+
+    public void setDapan2(String dapan2) {
+        this.dapan2 = dapan2;
+    }
+
+    public String getDapan3() {
+        return dapan3;
+    }
+
+    public void setDapan3(String dapan3) {
+        this.dapan3 = dapan3;
+    }
+
+    public String getDapan4() {
+        return dapan4;
+    }
+
+    public void setDapan4(String dapan4) {
+        this.dapan4 = dapan4;
+    }
+}
--- app/src/main/java/com/example/thietkegiaodien/view/chucai/KiemTra.java
+++ app/src/main/java/com/example/thietkegiaodien/view/chucai/KiemTra.java
@@ -1,13 +1,25 @@
 package com.example.thietkegiaodien.view.chucai;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.thietkegiaodien.R;
+import com.example.thietkegiaodien.view.convat.ConVat;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Random;
 
 public class KiemTra extends AppCompatActivity {
@@ -15,16 +27,119 @@ public class KiemTra extends AppCompatActivity {
             R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f,
             R.drawable.g, R.drawable.h, R.drawable.i, R.drawable.k};
 
-    Random random = new Random();
-    int ranNum = random.nextInt(10);
-private ImageView imageView;
+    public static String[] noiDung = {"Chữ A", "Chữ B",
+            "Chữ C", "Chữ D", "Chữ E", "Chữ F",
+            "Chữ G", "Chữ H", "Chữ I", "Chữ K"};
+
+    private Button buttonA;
+    private Button buttonB;
+    private Button buttonC;
+    private Button buttonD;
+
+    private ImageView imageView;
+
+    private List<Cauhoi> list = new ArrayList<>();
+
+    private void showAlert(String s) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(KiemTra.this, R.style.AlertDialog);
+        View view = LayoutInflater.from(KiemTra.this).inflate(
+                R.layout.alert, findViewById(R.id.layoutAlert)
+        );
+        builder.setView(view);
+
+        TextView textViewTitle = view.findViewById(R.id.textViewTitle);
+                Button buttonOK = view.findViewById(R.id.buttonOK);
+
+
+        textViewTitle.setText(s);
+        buttonOK.setText("OK");
+
+
+
+        AlertDialog alertDialog = builder.create();
+
+        buttonOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.dismiss();
+            }
+        });
+
+        if(alertDialog.getWindow() != null) {
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+
+        alertDialog.show();
+
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kiemtra_activity);
 
+
+        for(int i = 0;  i < 10; i++){
+            Random random = new Random();
+            int ranNum = random.nextInt(7);
+            Cauhoi cauhoi = new Cauhoi();
+            cauhoi.setNoidung(img[ranNum]);
+            cauhoi.setDapan1(noiDung[ranNum]);
+            cauhoi.setDapan2(noiDung[ranNum+1]);
+            cauhoi.setDapan3(noiDung[ranNum+2]);
+            cauhoi.setDapan4(noiDung[ranNum+3]);
+            list.add(cauhoi);
+        }
+        Random random = new Random();
+        int ranNum = random.nextInt(10);
+
         imageView = findViewById(R.id.image);
-        imageView.setImageResource(img[ranNum]);
+        imageView.setImageResource(list.get(ranNum).getNoidung());
+
+        buttonA = findViewById(R.id.buttonA);
+        buttonB = findViewById(R.id.buttonB);
+        buttonC = findViewById(R.id.buttonC);
+        buttonD = findViewById(R.id.buttonD);
+
+        buttonA.setText(list.get(ranNum).getDapan1());
+        buttonB.setText(list.get(ranNum).getDapan2());
+        buttonC.setText(list.get(ranNum).getDapan3());
+        buttonD.setText(list.get(ranNum).getDapan4());
+
+        buttonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showAlert("Đúng");
+                Random random = new Random();
+                int ranNum = random.nextInt(10);
+                imageView.setImageResource(list.get(ranNum).getNoidung());
+                buttonA.setText(list.get(ranNum).getDapan1());
+                buttonB.setText(list.get(ranNum).getDapan2());
+                buttonC.setText(list.get(ranNum).getDapan3());
+                buttonD.setText(list.get(ranNum).getDapan4());
+            }
+        });
+
+        buttonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAlert("Sai");
+            }
+        });
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAlert("Sai");
+            }
+        });
+        buttonD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAlert("Sai");
+            }
+        });
+
 
     }
 }
